c2f6b134bc7295d5a7291f562ccc37054d4ee0ea
vandeurenglenn <vandeurenglenn@gmail.com>
2019-07-14 23:22:50 +0200

    :+1:  add autocomplete & name

--- custom-input.js
+++ custom-input.js
@@ -1,12 +1,18 @@
 export default (() => {
   class CustomInput extends HTMLElement {
     static get observedAttributes() {
-      return ['placeholder', 'value', 'type'];
+      return ['placeholder', 'value', 'type', 'autocomplete', 'name'];
     }
     constructor() {
       super();
       this.attachShadow({mode: 'open'});
       this.shadowRoot.innerHTML = this.template;
+    }
+    set autocomplete(val) {
+      this.input.setAttribute('autocomplete', val);
+    }
+    set name(val) {
+      this.input.setAttribute('name', val);
     }
     set type(val) {
       this.input.setAttribute('type', val);
@@ -17,14 +23,20 @@ export default (() => {
     set value(val) {
       this.input.setAttribute('value', val);
     }
+    get autocomplete() {
+      return this.input.autocomplete;
+    }
     get input() {
       return this.shadowRoot.querySelector('input');
     }
     get value() {
       return this.input.value;
     }
+    get name() {
+      return this.input.name;
+    }
     addListener(name, cb) {
-      if(name === 'input' || name === 'change') {
+      if(name === 'input' || name === 'change' || name === 'value') {
         this.input.addEventListener(name, cb);
       } else {
         this.addEventListener(name, cb);
